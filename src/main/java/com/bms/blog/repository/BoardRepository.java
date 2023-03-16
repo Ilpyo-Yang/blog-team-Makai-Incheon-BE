52881966a413489242543db571943a976e3879bf
Ilpyo-Yang <ilpyoyang@gmail.com>
2023-03-16 13:45:58 +0900

    repository create

---
+++ src/main/java/com/bms/blog/repository/BoardRepository.java
@@ -0,0 +1,10 @@
+package com.bms.blog.repository;
+
+import com.bms.blog.entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
+
