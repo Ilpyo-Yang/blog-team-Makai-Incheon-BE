dbc2ea8586e9192d23cee6da56e16b076c049c6f
Ilpyo-Yang <ilpyoyang@gmail.com>
2023-03-16 14:01:43 +0900

    Tag 매핑 추가/삭제

---
+++ src/main/java/com/bms/blog/service/TagService.java
@@ -0,0 +1,25 @@
+package com.bms.blog.service;
+
+import com.bms.blog.entity.Tag;
+import com.bms.blog.repository.TagRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TagService {
+    private final TagRepository tagRepository;
+    ModelMapper modelMapper = new ModelMapper();
+
+    @Transactional
+    public List<Tag> getTag(){ return tagRepository.findAll(); }
+
+    @Transactional
+    public void setTag(String tag) { tagRepository.save(modelMapper.map(tag, Tag.class)); }
+
+}
