a34f8ab06015e00352cb92900b82761fdfbefcaf
Ilpyo-Yang <ilpyoyang@gmail.com>
2023-04-07 15:25:58 +0900

    exception class 수정

--- src/main/java/com/bms/blog/exception/ResourceNotFoundException.java
+++ src/main/java/com/bms/blog/exception/ResourceNotFoundException.java
@@ -1,9 +1,13 @@
 package com.bms.blog.exception;
 
 import com.bms.blog.dto.ResponseDto;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@Getter
+@Setter
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     private transient ResponseDto responseDto;
@@ -17,22 +21,6 @@ public class ResourceNotFoundException extends RuntimeException {
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
-    }
-
-    public String getResourceName() {
-        return resourceName;
-    }
-
-    public String getFieldName() {
-        return fieldName;
-    }
-
-    public Object getFieldValue() {
-        return fieldValue;
-    }
-
-    public ResponseDto getApiResponse() {
-        return responseDto;
     }
 
     private void setApiResponse() {
